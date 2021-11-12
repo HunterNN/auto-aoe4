dbbb590a0a39bf0c02be6d234ea1ae72d6a9009a
Stephan Kessler <stephan.kessler@outlook.com>
2021-11-12 15:16:56 +0100

    added python script

---
+++ auot.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import tkinter, tkinter.messagebox
+import os
+
+REDIST_FILE_URL = "https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe"
+REDIST_FILE_NAME = REDIST_FILE_URL.split("/")[-1]
+CABINET_FILE = "a10"
+DLL_FILE = "ucrtbase.dll"
+SYSTEM32_SUB_PATH = "steam/steamapps/compatdata/1466860/pfx/drive_c/windows/system32"
+TMP_PATH = "/tmp"
+
+def messagebox(title, text):
+    root = tkinter.Tk()
+    root.withdraw()
+    tkinter.messagebox.showinfo(title, text)
+    root.destroy()
+
+def errorbox(title, text):
+    root = tkinter.Tk()
+    root.withdraw()
+    tkinter.messagebox.showerror(title, text)
+    root.destroy()
+
+def getAndCheckSystemFolder():
+    global SYSTEM32_SUB_PATH
+    path = os.path.join(os.environ['HOME'], '.steam')
+    if not os.path.isdir(path):
+        errorbox("No Steam Folder", f"Steam path does not exist: {path}")
+        return
+    path = os.path.join(path, SYSTEM32_SUB_PATH)
+    if not os.path.isdir(path):
+        errorbox("No System32 Folder", f"Can't find system folder. Is the game installed with proton?")
+        return
+    return path
+
+def downloadFile(url, dest_path):
+    os.system(f'wget -c --read-timeout=5 --tries=0 "{url}" -P "{dest_path}"')
+
+def extractFile(folder_path, exe_name, cabinet_name):
+    os.system(f'cabextract "{os.path.join(folder_path, exe_name)}"')
+    os.system(f'cabextract "{os.path.join(folder_path, cabinet_name)}"')
+
+def checkFiles(path_1, path_2, file_name):
+    sum_1 = os.popen(f'md5sum "{os.path.join(path_1, file_name)}"').read()
+    messagebox("", sum_1)
+
+system32_path = getAndCheckSystemFolder()
+downloadFile(REDIST_FILE_URL, TMP_PATH)
+extractFile(TMP_PATH, REDIST_FILE_NAME, CABINET_FILE)
+checkFiles(TMP_PATH, system32_path, DLL_FILE)
